cada6efffd974c1b489297dbc7362f265bd95876
aruningole70 <aruningole70@gmail.com>
2024-08-15 12:13:21 +0530

    addesd stying to application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { StringClaculator } from "./StringClaculator"
+import { Card, CardContent , TextField , Button, Grid , Typography} from '@mui/material';
 
 function App() {
 
@@ -20,17 +21,34 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>String Calculator</h1>
-      <form onSubmit={handleSubmit}>
-        <input
+      <Card>
+        <h1>String Calculator</h1>
+        <CardContent>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={4}>
+            <Grid item sm={8}>
+            <TextField
           type="text"
+          variant="outlined" 
+          fullWidth
           value={formData}
           onChange={(e) => setFormData(e.target.value)}
-          placeholder="Enter numbers"
+          label="Enter numbers"
         />
-        <button type="submit">Calculate</button>
+            </Grid>
+            <Grid item sm={8} container justifyContent="flex-end">
+            <Button type="submit" variant="contained">Calculate</Button>
+            </Grid>
+            <Grid item sm={8} container justifyContent="center">
+            <Typography variant="h1" component="h2">
+            {error ? <p style={{ color: 'red' }}>{error}</p> : <h6>Sum: {result}</h6>}
+</Typography>
+            
+            </Grid>
+          </Grid>
       </form>
-      {error ? <p style={{ color: 'red' }}>{error}</p> : <h2>Sum: {result}</h2>}
+        </CardContent>
+      </Card>
     </div>
   );
 }
